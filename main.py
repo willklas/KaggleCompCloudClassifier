99d4c0b219b3ca497e0e2dd5bb0a5ca3f154142f
Will <william_klassen@hotmail.com>
2019-10-01 21:13:14 -0400

    update, images still not showing though

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from glob import glob
 
 import albumentations as albu
 import keras
+from keras.models import load_model
 from keras import backend as K
 from keras.models import Model
 from keras.layers import Input, UpSampling2D, Conv2D, Activation
@@ -24,7 +25,17 @@ from keras.losses import binary_crossentropy
 from keras.callbacks import Callback, ModelCheckpoint
 from keras import optimizers
 
+
 from sklearn.model_selection import train_test_split
+
+
+# global variables
+batch_size       = 16
+img_size         = (256, 256, 3)
+tr               = pd.read_csv("input/train.csv")
+img_names_all    = tr['Image_Label'].apply(lambda x: x.split('_')[0]).unique()
+steps_per_epoch  = 200             
+epochs           = 20  
 
 
 def rle2mask(rle, imgshape):
@@ -61,6 +72,7 @@ def dice_loss(y_true, y_pred):
     score = (2. * K.sum(intersection) + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
     return 1. - score
 
+
 def bce_dice_loss(y_true, y_pred):
     return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
 
@@ -84,12 +96,12 @@ def keras_generator(batch_size):
             masks = []
             for rle in tr[tr['Image_Label'].apply(lambda x: x.split('_')[0]) == fn]['EncodedPixels']:                
                 if pd.isnull(rle):
-                    mask = np.zeros((img_size, img_size))
+                    mask = np.zeros((img_size[0], img_size[1]))
                 else:
                     mask = rle2mask(rle, img.shape)
-                    mask = cv2.resize(mask, (img_size, img_size))
+                    mask = cv2.resize(mask, (img_size[0], img_size[1]))
                 masks.append(mask)                                        
-            img = cv2.resize(img, (img_size, img_size))            
+            img = cv2.resize(img, (img_size[0], img_size[1]))            
             x_batch += [img]
             y_batch += [masks] 
 
@@ -100,79 +112,95 @@ def keras_generator(batch_size):
 
         yield x_batch, y_batch
 
+def create_model():
+    BACKBONE = 'resnet50'
+    preprocess_input = sm.backbones.get_preprocessing(BACKBONE)
+
+    model = sm.Unet(
+            encoder_name  = BACKBONE, 
+            classes       = 4,
+            activation    = 'sigmoid',
+            input_shape   = img_size)
+    
+    model.compile(optimizer=optimizers.Adam(lr=9e-3), loss=bce_dice_loss)
+
+    return model
+
+
 class EpochBegin(keras.callbacks.Callback):
     def on_epoch_begin (self, epoch, logs={}):
         global new_ep
         new_ep = True
 
 
-img_size = 256
-tr = pd.read_csv("input/train.csv")
+def main(model_name):
+    ######### TRAINING  #########
 
-print(len(tr))
-print(tr.head())
+    print("\nnumber of segmentation definitions:", len(tr), "(preview seen below)")
+    print(tr.head())
 
-img_names_all = tr['Image_Label'].apply(lambda x: x.split('_')[0]).unique()
-print(len(img_names_all))
+    print("\nnumber of unique images:", len(img_names_all))
 
-BACKBONE = 'resnet50'
-preprocess_input = sm.backbones.get_preprocessing(BACKBONE)
+    print("creating model...")
+    model = create_model()
 
-model = sm.Unet(
-           encoder_name=BACKBONE, 
-           classes=4,
-           activation='sigmoid',
-           input_shape=(img_size, img_size, 3))
+    call_back_list = [EpochBegin()]
 
-model.compile(optimizer=optimizers.Adam(lr=9e-3), loss=bce_dice_loss)
+    print("training model...")
+    model.fit_generator(keras_generator(batch_size),
+                        steps_per_epoch  = steps_per_epoch,              
+                        epochs           = epochs,                    
+                        verbose          = 1,
+                        callbacks        = call_back_list)
 
-Epoch_Begin_Clb = EpochBegin()
+    print("saving trained model...")
+    model.save("models/" + model_name + ".h5")
 
-batch_size = 16
-# model.fit_generator(keras_generator(batch_size),
-#               steps_per_epoch=200,              
-#               epochs=20,                    
-#               verbose=1,
-#               callbacks=[Epoch_Begin_Clb]
-#               )
+    gc.collect()
 
-gc.collect()
+    ######### PREDICTING #########
 
-test_img = []
-# testfiles=os.listdir('input/test/*.jpg')
-testfiles = glob("input/test/*.jpg") 
-print("--------------------here------------------------")
-for image_path in tqdm(testfiles):    
-    # print(fn)
-    # input() 
-    img = cv2.imread(image_path)
-    img = cv2.resize(img,(img_size,img_size))       
-    test_img.append(img)
+    # print("loading model")
+    # model = load_model("models/" + model_name + ".h5")
 
-print(len(test_img))
+    test_images = []
+    test_images_paths = glob("input/test/*.jpg") 
 
-predict = model.predict(np.asarray(test_img))
+    print("reading in test images...")
+    for image_path in tqdm(test_images_paths):    
+        img = cv2.imread(image_path)
+        img = cv2.resize(img,(img_size[0],img_size[1]))       
+        test_images.append(img)
 
-pred_rle = []
-for img in predict:      
-    img = cv2.resize(img, (525, 350))
-    tmp = np.copy(img)
-    tmp[tmp<np.mean(img)] = 0
-    tmp[tmp>0] = 1
-    for i in range(tmp.shape[-1]):
-        pred_rle.append(mask2rle(tmp[:,:,i]))
+    print("length of test images:", len(test_images))
 
-print(len(pred_rle))
+    print("predicting test image segmentations...")
+    predict = model.predict(np.asarray(test_images))
 
-# show results of prediction
-fig, axs = plt.subplots(5, figsize=(20, 20))
-axs[0].imshow(cv2.resize(plt.imread(testfiles[0]),(525, 350)))
-for i in range(4):
-    axs[i+1].imshow(rle2mask(pred_rle[i], img.shape))
+    pred_rle = []
+    for img in tqdm(predict):      
+        img = cv2.resize(img, (525, 350))
+        tmp = np.copy(img)
+        tmp[tmp<np.mean(img)] = 0
+        tmp[tmp>0] = 1
+        for i in range(tmp.shape[-1]):
+            pred_rle.append(mask2rle(tmp[:,:,i]))
 
-# create submission file
-sub = pd.read_csv('sample_submission.csv', converters={'EncodedPixels': lambda e: ' '} )
-sub['EncodedPixels'] = pred_rle
-sub.head()
+    print(len(pred_rle))
 
-sub.to_csv('submissions/submission.csv', index=False)
+    # show results of prediction
+    fig, axs = plt.subplots(5, figsize=(20, 20))
+    axs[0].imshow(cv2.resize(plt.imread(test_images_paths[0]),(525, 350)))
+    for i in range(4):
+        axs[i+1].imshow(rle2mask(pred_rle[i], img.shape))
+
+    # create submission file
+    sub = pd.read_csv('sample_submission.csv', converters={'EncodedPixels': lambda e: ' '} )
+    sub['EncodedPixels'] = pred_rle
+    sub.head()
+
+    sub.to_csv('submissions/submission.csv', index=False)
+
+
+if __name__ == "__main__":
+    main(model_name = "model_1")
